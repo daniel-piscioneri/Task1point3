e8c3a11b16f264acae93027c4f165e6cf9c35e9f
Daniel Piscioneri <danielp2811@gmail.com>
2023-10-05 12:32:18 +1100

    message

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -31,7 +31,7 @@
 <script setup>
 import { ref } from 'vue';
 
-import projectImage1 from '../assets/images/screenshot.jpeg';
+import projectImage1 from '../assets/images/378265135_2342852519220661_2360878974908914406_n.jpg';
 import projectImage2 from '../assets/images/screenshot2.jpeg';
 
 
